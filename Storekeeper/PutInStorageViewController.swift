79a5f25e8650d393cbd483212fffcf38c91482f0
jjjjjeffrey <jjjjjeffreyzeng@gmail.com>
2017-07-12 17:50:30 +0800

    add Put In Storage view

---
+++ Storekeeper/PutInStorageViewController.swift
@@ -0,0 +1,48 @@
+//
+//  PutInStorageViewController.swift
+//  Storekeeper
+//
+//  Created by zengdaqian on 2017/7/12.
+//  Copyright © 2017年 zengdaqian. All rights reserved.
+//
+
+import UIKit
+
+class PutInStorageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension PutInStorageViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        return cell
+    }
+    
+}
