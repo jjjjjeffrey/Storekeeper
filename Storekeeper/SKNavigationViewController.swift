5bc5df1d77eb7308c0f7a7329045b62c1f885d82
jjjjjeffrey <jjjjjeffreyzeng@gmail.com>
2017-07-20 10:56:58 +0800

    add member

---
+++ Storekeeper/SKNavigationViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SKNavigationViewController.swift
+//  Storekeeper
+//
+//  Created by zengdaqian on 2017/7/20.
+//  Copyright © 2017年 zengdaqian. All rights reserved.
+//
+
+import UIKit
+
+class SKNavigationViewController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = UIColor.white
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
