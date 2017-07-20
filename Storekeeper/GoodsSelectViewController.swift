81f2c99330e80b7fb7862f2e086d598766245153
jjjjjeffrey <jjjjjeffreyzeng@gmail.com>
2017-07-20 10:32:53 +0800

    add GoodsSelectViewController and GoodsSelectedPoolViewController

--- Storekeeper/GoodsSelectViewController.swift
+++ Storekeeper/GoodsSelectViewController.swift
@@ -1,15 +1,14 @@
 //
-//  PutInStorageViewController.swift
+//  GoodsSelectViewController.swift
 //  Storekeeper
 //
-//  Created by zengdaqian on 2017/7/12.
+//  Created by zengdaqian on 2017/7/13.
 //  Copyright © 2017年 zengdaqian. All rights reserved.
 //
 
 import UIKit
-import PKHUD
 
-class PutInStorageViewController: UIViewController {
+class GoodsSelectViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,27 +31,29 @@ class PutInStorageViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-    
-    @IBAction func confirmButtonClicked(_ sender: Any) {
-        
-        HUD.flash(.labeledSuccess(title: "入库成功", subtitle: nil), delay: 1.0) { (success) in
-            self.navigationController?.popToRootViewController(animated: true)
-        }
-        
-    }
-    
 
 }
 
-extension PutInStorageViewController: UITableViewDataSource {
+extension GoodsSelectViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return 5
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "SomeLongIdentifierCell", for: indexPath)
         return cell
+    }
+    
+}
+
+extension GoodsSelectViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //解决cell点击事件延迟的bug
+        tableView.deselectRow(at: indexPath, animated: false)
+        
+        
     }
     
 }
--- Storekeeper/GoodsSelectedPoolViewController.swift
+++ Storekeeper/GoodsSelectedPoolViewController.swift
@@ -1,15 +1,15 @@
 //
-//  PutInStorageViewController.swift
+//  GoodsSelectedPoolViewController.swift
 //  Storekeeper
 //
-//  Created by zengdaqian on 2017/7/12.
+//  Created by zengdaqian on 2017/7/13.
 //  Copyright © 2017年 zengdaqian. All rights reserved.
 //
 
 import UIKit
-import PKHUD
+import IBAnimatable
 
-class PutInStorageViewController: UIViewController {
+class GoodsSelectedPoolViewController: AnimatableModalViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,26 +32,18 @@ class PutInStorageViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-    
-    @IBAction func confirmButtonClicked(_ sender: Any) {
-        
-        HUD.flash(.labeledSuccess(title: "入库成功", subtitle: nil), delay: 1.0) { (success) in
-            self.navigationController?.popToRootViewController(animated: true)
-        }
-        
-    }
-    
 
 }
 
-extension PutInStorageViewController: UITableViewDataSource {
+extension GoodsSelectedPoolViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return 8
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
         return cell
     }
     
